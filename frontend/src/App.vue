4e82dfb7ada0aef77d2dafdd0c35e19ed259cf87
Rens Saladaga <staleexam19@gmail.com>
2026-03-01 14:47:14 +0800

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-screen justify-center items-center">
+    <h1>You did it!</h1> 
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
