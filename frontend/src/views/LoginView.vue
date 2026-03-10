92d39f55fe8a7c142f650477358b621ec945ae05
Rens Saladaga <staleexam19@gmail.com>
2026-03-10 17:03:25 +0800

    Added login page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-    <main>
-        <div class="container mx-auto">
-            <form >
-                
-            </form>
-        </div>
+    <main class="min-h-screen flex justify-center items-center">
+        <form class="w-xs">
+            <p class="text-lg font-semibold">Sign In</p>
+            <br>
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend">Email</legend>
+                <input type="text" class="input" />
+                <p class="label"></p>
+            </fieldset>
+
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend">Password</legend>
+                <input type="password" class="input" />
+                <p class="label"></p>
+            </fieldset>
+
+            <button class="btn btn-primary w-full">Sign In</button>
+        </form>
     </main>
 </template>
